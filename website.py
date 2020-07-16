2382f4d30d11a2094796f9e1f2893db567ae9af7
Pratik Badhe <43951072+pratikbadhe@users.noreply.github.com>
2020-07-16 21:43:41 +0530

    Update website.py

--- website.py
+++ website.py
@@ -3,6 +3,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def index():
-	title = 'site'
+	title = 'Team Endurance Racing'
 
-	return render_template('layout/master.html',title=title)
+	return render_template('layout/index.html',title=title)
